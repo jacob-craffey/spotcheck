b0dc5c36efeeeeb33693da054119095b1db7ff4c
Jacob Craffey <me@pop-os.localdomain>
2020-04-26 17:04:49 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SpotDialogComponent } from './components/spot-dialog/spot-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import apiKey from '../environments/api-keys.json';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpotDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: apiKey["google-maps"]
+    }),
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/spot-dialog/spot-dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-spot-dialog',
+  templateUrl: './spot-dialog.component.html',
+  styleUrls: ['./spot-dialog.component.scss']
+})
+export class SpotDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<SpotDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
+
+export interface DialogData {
+  name: string;
+  images: string[];
+}
+
