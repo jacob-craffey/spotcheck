9f18dd8536eb07390af8c763598ce878edc3693a
Jacob Craffey <me@pop-os.localdomain>
2020-04-29 20:30:37 -0400

    Navbar design created

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  toolTipMessage = 'Login or create an account to contribute to the community!';
   constructor() { }
 
   ngOnInit(): void {
